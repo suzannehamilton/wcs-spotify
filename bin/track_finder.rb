b20fbb09a38e71b6cfa74c14f8d5b821ce332162
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-02-05 23:34:52 +0000

    Fetch all West Coast Swing playlists
    Search for playlists with "West Coast Swing" or "WCS" in the title or
    description.
    
    This currently has no de-duplication, and the search limit is hard-coded
    to the current number of results to test out what sort of data we can
    get from the Spotify API.

--- bin/track_finder.rb
+++ bin/track_finder.rb
@@ -1,8 +1,18 @@
 #!/usr/bin/env ruby
 
-require 'rspotify'
+require "rspotify"
 
-artists = RSpotify::Artist.search('Arctic Monkeys')
-arctic_monkeys = artists.first
+wcs_playlists = RSpotify::Playlist.search("WCS", limit: 50)
+wcs_playlists.concat(RSpotify::Playlist.search("WCS", limit: 50, offset: 50))
+wcs_playlists.concat(RSpotify::Playlist.search("WCS", limit: 50, offset: 100))
+wcs_playlists.concat(RSpotify::Playlist.search("WCS", limit: 50, offset: 150))
+wcs_playlists.concat(RSpotify::Playlist.search("WCS", limit: 50, offset: 200))
+wcs_playlists.concat(RSpotify::Playlist.search("West Coast Swing", limit: 50))
+wcs_playlists.concat(RSpotify::Playlist.search("West Coast Swing", limit: 50, offset: 50))
+wcs_playlists.concat(RSpotify::Playlist.search("West Coast Swing", limit: 50, offset: 100))
 
-puts arctic_monkeys.name
+puts wcs_playlists.length
+
+wcs_playlists.each do |playlist|
+  puts "'#{playlist.name}' by '#{playlist.owner.id}'. #{playlist.total} tracks"
+end
