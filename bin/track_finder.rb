bc24f1185317da6a02912903e625ea2eb8a41caf
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2017-02-20 22:35:51 +0000

    Automate search result paging
    Replace the temporary hard-coded search paging with iterations over
    the full search results.

--- bin/track_finder.rb
+++ bin/track_finder.rb
@@ -2,14 +2,37 @@
 
 require "rspotify"
 
-wcs_playlists = RSpotify::Playlist.search("WCS", limit: 50)
-wcs_playlists.concat(RSpotify::Playlist.search("WCS", limit: 50, offset: 50))
-wcs_playlists.concat(RSpotify::Playlist.search("WCS", limit: 50, offset: 100))
-wcs_playlists.concat(RSpotify::Playlist.search("WCS", limit: 50, offset: 150))
-wcs_playlists.concat(RSpotify::Playlist.search("WCS", limit: 50, offset: 200))
-wcs_playlists.concat(RSpotify::Playlist.search("West Coast Swing", limit: 50))
-wcs_playlists.concat(RSpotify::Playlist.search("West Coast Swing", limit: 50, offset: 50))
-wcs_playlists.concat(RSpotify::Playlist.search("West Coast Swing", limit: 50, offset: 100))
+search_terms = [
+  "wcs",
+  "westcoastswing",
+  "west coastswing",
+  "westcoast swing",
+  "west coast swing",
+]
+
+def search_playlists(search_term)
+  found_all_results = false
+  offset = 0
+  set_size = 50
+
+  results = []
+
+  while !found_all_results
+    puts "Searching for '#{search_term}' with offset #{offset}"
+
+    result_set = RSpotify::Playlist.search(search_term, limit: set_size, offset: offset)
+    results.concat(result_set)
+
+    offset += set_size
+    found_all_results = result_set.length < set_size
+  end
+
+  results
+end
+
+wcs_playlists = search_terms.map { |term|
+  search_playlists(term)
+}.flatten
 
 puts wcs_playlists.length
 
