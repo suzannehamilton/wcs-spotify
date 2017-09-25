50d0b1346bad406b36e5bed5072a63e3ebcbfced
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2017-09-25 16:34:52 +0200

    Test that monthly chart has the correct date

--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ gem 'thor', '~> 0.19.4'
 # Testing
 gem 'rspec', '~> 3.6'
 gem 'webmock', '~> 3.0.1'
+gem 'timecop', '~> 0.9.1'
--- spec/integration/track_fetcher_spec.rb
+++ spec/integration/track_fetcher_spec.rb
@@ -1,7 +1,9 @@
+require "timecop"
+
 require_relative "../../lib/track_fetcher"
 
 RSpec.describe TrackFetcher do
-  before(:all) do
+  before(:each) do
     stub_request(:post, "https://accounts.spotify.com/api/token")
       .with(body: {"grant_type"=>"client_credentials"})
       .to_return(status: 200, body: "{}", headers: {})
@@ -13,11 +15,25 @@ RSpec.describe TrackFetcher do
     stub_request(:get, /api.spotify.com\/v1\/search\?limit=50&offset=0&q=.+&type=playlist/)
       .to_return(status: 200, body: empty_results, headers: {})
 
+    # TODO: Extract to setup
     track_fetcher = TrackFetcher.new
     results = track_fetcher.fetch_tracks
 
     expect(results.yearly_tracks).to eq([])
     expect(results.monthly_tracks).to eq([])
+  end
+
+  it "generates a chart for the previous month" do
+    empty_results = File.read("spec/fixtures/playlist_search/no_results.json")
+    stub_request(:get, /api.spotify.com\/v1\/search\?limit=50&offset=0&q=.+&type=playlist/)
+      .to_return(status: 200, body: empty_results, headers: {})
+
+    track_fetcher = TrackFetcher.new
+    Timecop.freeze(DateTime.new(2015, 8, 24)) do
+      results = track_fetcher.fetch_tracks
+
+      expect(results.month_beginning).to eq(DateTime.new(2015, 7, 1))
+    end
   end
 
   # TODO: Test year and month are correct in results object
