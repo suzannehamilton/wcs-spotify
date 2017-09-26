79c411c929c32c5844817220dc3f0fcf41a87c7a
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2017-09-26 14:28:24 +0200

    Add unit tests to playlist searching

--- lib/spotify/playlist_search.rb
+++ lib/spotify/playlist_search.rb
@@ -1,30 +1,34 @@
 class PlaylistSearch
-  def initialize
+  def initialize(batch_size: 50)
+    @batch_size = batch_size
     @logger = Logging.logger[self]
   end
 
   def search_playlists(search_term)
     found_all_results = false
     offset = 0
-    set_size = 50
 
     results = []
 
     while !found_all_results
       @logger.info "Searching for '#{search_term}' with offset #{offset}"
 
-      result_set = RSpotify::Playlist.search(search_term, limit: set_size, offset: offset)
+      result_set = RSpotify::Playlist.search(search_term, limit: batch_size, offset: offset)
 
       filtered_results = result_set.select { |p| matches_term?(p, search_term) }
 
       results.concat(result_set)
 
-      offset += set_size
-      found_all_results = result_set.length < set_size
+      offset += batch_size
+      found_all_results = result_set.length < batch_size
     end
 
     results
   end
+
+private
+
+  attr_reader :batch_size
 
   # Spotify search results are too broad. For example, the search term "wcs"
   # matches playlists named "wc" which are very unlikely to be relevant. So only
---
+++ spec/unit/spotify/playlist_search_spec.rb
@@ -0,0 +1,64 @@
+require_relative "../../../lib/spotify/playlist_search"
+
+require "rspotify"
+
+RSpec.describe PlaylistSearch do
+  BATCH_SIZE = 3.freeze
+
+  before(:all) do
+    @playlist_search = PlaylistSearch.new(batch_size: BATCH_SIZE)
+  end
+
+  it "returns empty results if there are no matching playlists" do
+    rspotify_playlist = class_double("RSpotify::Playlist").as_stubbed_const
+    raw_results = []
+    allow(rspotify_playlist).to receive(:search)
+      .with("some search term", limit: BATCH_SIZE, offset: 0)
+      .and_return(raw_results)
+
+    search_results = @playlist_search.search_playlists("some search term")
+    expect(search_results).to eq([])
+  end
+
+  it "returns all playlists if there is a single page of results" do
+    raw_results = [
+      RSpotify::Playlist.new("name" => "Playlist 1", "tracks" => {}),
+      RSpotify::Playlist.new("name" => "Playlist 2", "tracks" => {}),
+    ]
+    rspotify_playlist = class_double("RSpotify::Playlist").as_stubbed_const
+    allow(rspotify_playlist).to receive(:search)
+      .with("some search term", limit: BATCH_SIZE, offset: 0)
+      .and_return(raw_results)
+
+    search_results = @playlist_search.search_playlists("some search term")
+    expect(search_results).to eq(raw_results)
+  end
+
+  it "combines all pages of results" do
+    playlist_1 = RSpotify::Playlist.new("name" => "Playlist 1", "tracks" => {})
+    playlist_2 = RSpotify::Playlist.new("name" => "Playlist 2", "tracks" => {})
+    playlist_3 = RSpotify::Playlist.new("name" => "Playlist 3", "tracks" => {})
+    playlist_4 = RSpotify::Playlist.new("name" => "Playlist 4", "tracks" => {})
+    playlist_5 = RSpotify::Playlist.new("name" => "Playlist 5", "tracks" => {})
+
+    page_1 = [playlist_1, playlist_2, playlist_3]
+    page_2 = [playlist_4, playlist_5]
+
+    rspotify_playlist = class_double("RSpotify::Playlist").as_stubbed_const
+    allow(rspotify_playlist).to receive(:search)
+      .with("some search term", limit: BATCH_SIZE, offset: 0)
+      .and_return(page_1)
+    allow(rspotify_playlist).to receive(:search)
+      .with("some search term", limit: BATCH_SIZE, offset: 3)
+      .and_return(page_2)
+
+    search_results = @playlist_search.search_playlists("some search term")
+    expect(search_results).to eq([
+      playlist_1,
+      playlist_2,
+      playlist_3,
+      playlist_4,
+      playlist_5,
+    ])
+  end
+end
