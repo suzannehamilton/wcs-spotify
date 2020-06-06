44ce26a2a86906e99d9e0bd320aa233a6e64acd8
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2020-06-06 17:05:48 +0100

    Deduplicate tracks by title and artist IDs

--- lib/canonical_track_finder.rb
+++ lib/canonical_track_finder.rb
@@ -1,7 +1,20 @@
 class CanonicalTrackFinder
   def find_tracks(playlist_data_file, output_file)
     playlist_data = CSV.read(playlist_data_file, headers: true)
-
     puts "Found #{playlist_data.length} lines"
+
+    grouped_tracks = Hash.new { |h, k| h[k] = [] }
+
+    playlist_data.each do |track|
+      key = {
+        # TODO: De-duplicate tracks with similar titles
+        name: track["full_name"],
+        artists: track["artist_ids"].split(",").sort
+      }
+
+      grouped_tracks[key] << track
+    end
+
+    puts "Found #{grouped_tracks.size} de-duplicated tracks"
   end
 end
