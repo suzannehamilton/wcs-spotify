de1d617266e25837cb02377736430e94adadef13
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2020-06-07 21:43:45 +0100

    Find tracks with imprecise earliest release dates
    It's useful to see these so I can decide what to do with them when generating
    charts by release date.

--- lib/canonical_track_finder.rb
+++ lib/canonical_track_finder.rb
@@ -22,10 +22,12 @@ class CanonicalTrackFinder
 
     puts "Found #{grouped_tracks.size} de-duplicated tracks"
 
-    grouped_tracks.take(10).each do |key, tracks|
+    grouped_tracks.each do |key, tracks|
       earliest_track = tracks.min_by { |track| release_date(track) }
 
-      puts "Earliest track of '#{key[:name]}' is '#{earliest_track['track_id']}' released on #{earliest_track['release_date']}"
+      if earliest_track["release_date_precision"] == "year" || earliest_track["release_date_precision"] == "month"
+        puts "Earliest track of '#{key[:name]}' is '#{earliest_track['track_id']}' released on #{earliest_track['release_date']}"
+      end
     end
   end
 
@@ -34,7 +36,10 @@ private
   def release_date(track)
     rough_release_date = track["release_date"]
 
-    if track["release_date_precision"] == "year"
+    if rough_release_date.nil?
+      # TODO: Don't hack the date: Only use this track as the canonical track if it's the only one
+      Date.today + 365
+    elsif track["release_date_precision"] == "year"
       Date.strptime(rough_release_date, "%Y").next_year.prev_day
     elsif track["release_date_precision"] == "month"
       Date.strptime(rough_release_date, "%Y-%m").next_month.prev_day
