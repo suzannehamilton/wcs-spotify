75987a0e12a7ba592f85c0a8009c821e543eb8e8
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2020-10-04 14:28:41 +0100

    Build hash of canonical track IDs

--- lib/chart_extractor.rb
+++ lib/chart_extractor.rb
@@ -5,9 +5,20 @@ class ChartExtractor
     start_date,
     end_date
   )
-
     playlist_data = CSV.read(playlist_data_file, headers: true)
     puts "Found #{playlist_data.length} lines in raw playlist data"
+
+    canonical_data = CSV.read(canonical_track_data_file, headers: true)
+    puts "Found #{canonical_data.length} lines in canonical track data"
+
+    canonical_tracks = Hash.new
+
+    canonical_data.each do |track|
+      track_id = track["track_id"]
+      canonical_tracks[track_id] = track
+    end
+
+    puts "Built track lookup hash of  #{canonical_data.length} tracks"
 
     count = 0
 
