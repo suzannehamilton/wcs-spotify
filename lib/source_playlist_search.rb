d5ed4da31a077b7c130e33302c1e354824162206
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2022-03-06 17:46:35 +0000

    Save list of playlist search results to a CSV
    This will make it possible to build up playlists from month to month,
    and find good new search terms. This is useful now that Spotify limits
    the number of results returned by the search API.

--- lib/source_playlist_search.rb
+++ lib/source_playlist_search.rb
@@ -1,11 +1,53 @@
+require_relative "spotify/playlist_search"
+
 class SourcePlaylistSearch
+  def initialize
+    @logger = Logging.logger[self]
+    @playlist_search = PlaylistSearch.new
+  end
+
   def find_playlists(output_path)
+    search_terms = [
+      "wcs",
+      # "westcoastswing",
+      # "west coastswing",
+      # "westcoast swing",
+      # "west coast swing",
+      # "westie",
+    ]
+
+    # TODO: Reference file relative to this one?
+    config = YAML::load_file("config.yaml")
+
+    RSpotify.authenticate(config["spotify_api"]["client_id"], config["spotify_api"]["client_secret"])
+
+    # TODO: Deduplicate playlist IDs
+    wcs_playlists = search_terms.map { |term|
+      @playlist_search.search_playlists(term)
+    }.flatten
+
     CSV.open(output_path, "wb") do |csv|
       csv << [
         "id",
+        "track_count",
         "name",
         "description",
       ]
+
+      wcs_playlists.each do |playlist|
+        csv << [
+          playlist.id,
+          playlist.total,
+          playlist.name,
+          playlist.description,
+        ]
+      end
+
+      logger.info "Found #{wcs_playlists.length} playlists"
     end
   end
+
+  private
+
+    attr_reader :logger
 end
