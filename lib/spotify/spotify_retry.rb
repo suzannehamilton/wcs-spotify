1797da6358c507b758bcdfaac8f84172158880c6
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2024-05-06 17:13:02 +0100

    Add `Errno::ECONNRESET` to errors to retry on

--- lib/spotify/spotify_retry.rb
+++ lib/spotify/spotify_retry.rb
@@ -2,7 +2,7 @@ require 'retriable'
 
 class SpotifyRetry
   def self.retry
-    Retriable.retriable on: [RestClient::RequestTimeout, RestClient::BadGateway, RestClient::InternalServerError, Errno::EHOSTUNREACH], tries: 3 do
+    Retriable.retriable on: [RestClient::RequestTimeout, RestClient::BadGateway, RestClient::InternalServerError, Errno::EHOSTUNREACH, Errno::ECONNRESET], tries: 3 do
       yield
     end
   end
